0d3a132b57b40240baf9031329302cada71713b8
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-07-27 10:29:53 +0000

    Comparision

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,16 @@
+//Compairing two different DataTypes is not a good practice
+// console.log("2" > 1); 
+// console.log("20" < 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//Strict Check === 
+
+console.log("2" == 2);
