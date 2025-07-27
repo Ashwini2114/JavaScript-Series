07b3c557670a867214aacd3dac111b2a4c8b3171
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-07-27 11:01:36 +0000

    Memory Management in JavaScript

--- 01_Basics/dataTypes-summary.js
+++ 01_Basics/dataTypes-summary.js
@@ -36,3 +36,26 @@ console.log(typeof myFunction); //function
 
 console.log(typeof heros); //object
 console.log(typeof myObj); //object
+
+
+// Primitive Stack mn jaega aur iske andar hume copy milta hai
+// let a = 10
+// let b = a
+// b= 20
+// console.log(a); //10
+// console.log(b); //20
+
+
+// Non-Primitive Heap mn jaega aur yaha reference milta hai i.e Changes will be done in originbal value
+
+let user1 = {
+    email : "google.com",
+    password : 1416524
+}
+
+let user2 = user1
+
+user2.email = "youtube.com"
+
+console.log(user1.email); //youtube.com
+console.log(user2.email); //youtube.com
