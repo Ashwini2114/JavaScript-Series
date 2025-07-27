c3ed6d5fd98022189b470d47dfb8b5e37fe6a35a
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-07-27 09:34:49 +0000

    Variables

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //Treat all JS code as newer version
+
+// alert(3+3)  //We are using NodeJs, no browser
+
+console.log(3 + 3);
+
+
+let name = "ashwini"
+let age = 22
+let isLoggedIn = false
+
+// Number => 2 to power 53
+// BigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => Unique
+
+//Object
+
+console.log(typeof(age));
+console.log(typeof undefiend);
+console.log(typeof null);
+console.log(typeof NaN);
