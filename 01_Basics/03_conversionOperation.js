fc8b1919b04bbb0d2ef87966a0c93d936242a97f
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-07-27 09:59:06 +0000

    Conversion of DataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = null;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 0 false => 0
+// undefined => NaN
+// null => 0
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn  = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ashwini" => true
+
+let num = 783
+
+let strNum = String(num)
+
+console.log(strNum);
+console.log(typeof strNum);
+
+
